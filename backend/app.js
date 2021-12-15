bc99976e6a01a764244095e907ac4f1c57d78daf
jextaposition <alexa.joy.hayes@gmail.com>
2021-12-15 13:09:19 -0800

    first commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+const encouragementsRouter = require('./routes/encouragements');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+app.use('encouragements', encouragementsRouter);
+
+module.exports = app;
---
+++ frontend/src/data.js
@@ -0,0 +1,21 @@
+const data = {
+  encouragements: [
+    {
+      id: 1,
+      date: 'Monday, December 13, 2021',
+      scripture: 'Psalm 34',
+      scriptureText: 'some text',
+      encouragement: 'this is the encouragement',
+      prayer: 'this is my prayer',
+    },
+    {
+      id: 2,
+      date: 'Monday, December 13, 2021',
+      scripture: 'Psalm 34',
+      scriptureText: 'some text',
+      encouragement: 'this is the encouragement',
+      prayer: 'this is my prayer',
+    },
+  ],
+};
+export default data;
