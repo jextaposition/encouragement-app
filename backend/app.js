6bcfaaa0121a988a69ccc4e570e6356f80818993
jextaposition <alexa.joy.hayes@gmail.com>
2021-12-15 15:39:51 -0800

    created routes and models in backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const models = require('./models');
 
 const usersRouter = require('./routes/users');
 const encouragementsRouter = require('./routes/encouragements');
@@ -13,7 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+models.sequelize.sync({ alter: true }).then(function () {
+  console.log("DB Sync'd up");
+});
+
 app.use('/users', usersRouter);
-app.use('encouragements', encouragementsRouter);
+app.use('/encouragements', encouragementsRouter);
 
 module.exports = app;
---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      this.hasMany(models.Encouragement, {
+        foreignKey: 'id',
+      });
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
+};
